7f810e7c69185242d4845c2ac55e479227624e9f
Maxime Becker <mbecker@maximes-macbook-pro.home>
2021-06-30 14:00:21 +0200

    Sitev2

---
+++ Hackathon2/src/Controller/ProfilController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ProfilController
+ * @package App\Controller
+ * @Route("/profil", name="profil_")
+ */
+
+class ProfilController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('profil/index.html.twig',
+            ['profil' => ' ',]);
+    }
+}
